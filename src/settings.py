cd02b1d50ab85a781c0ed747f220a1518e389a85
callistachang <rezinomrah@gmail.com>
2019-06-28 16:18:44 +0800

    MEDIA ASSETS - configure settings and storage_backends

--- src/settings.py
+++ src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'src.core',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'src.storage_backends.MediaStorage'
